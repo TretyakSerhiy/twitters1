14c15f56fa28931e583f9f3fd10d888eb4e2bc31
TretyakSerhiy <ceed162@gmail.com>
2020-05-21 05:10:11 -0700

    #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'twitters1.herokuapp.com'
-  config.action_mailer.default_url_options = { host: https://twitters1.herokuapp.com}
+  config.action_mailer.default_url_options = { host: twitters1.herokuapp.com}
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
