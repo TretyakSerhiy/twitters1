ff3ed2b3683f6fd408febce3b1c96f2d89a71b08
Ruslan Oliinyk <russ.oliinyk@gmail.com>
2020-05-25 09:33:20 +0300

    Hotfix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,11 +4,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
+  # if Rails.env.production?
+  #   storage :fog
+  # else
     storage :file
-  end
+  # end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
